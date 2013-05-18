c840b7a5e56bde7764c74b20d8c69f729556c6e0
Joe Friedl <E001868@dealertrack.com>
2013-05-18 19:14:12 -0400

    Make it draggy

---
+++ static/mark.js
@@ -0,0 +1,21 @@
+function mark() {
+  var dragX, dragY,
+      guy = document.querySelector('#avatar');
+
+  guy.addEventListener('dragstart', function(e) {
+    dragX = e.x - e.target.x,
+    dragY = e.y - e.target.y;
+  });
+
+  window.addEventListener('drop', function(e) {
+    document.body.appendChild(guy);
+    guy.className = '';
+    guy.style.position = 'absolute';
+    guy.style.left = (e.x - dragX) + 'px';
+    guy.style.top = (e.y - dragY) + 'px';
+  });
+
+  window.addEventListener('dragover', function(e) {
+    e.preventDefault();
+  });
+}
