6bb289a7eb0b539974295d435b257823df30321c
Joe Friedl <E001868@dealertrack.com>
2013-05-25 18:20:35 -0400

    Save and display marks

--- joefriedl/static/mark.js
+++ joefriedl/static/mark.js
@@ -1,22 +1,26 @@
 function mark() {
   var dragX, dragY,
-      guy = document.querySelector('#avatar');
+      guy = $('#avatar'),
+      marks = $('#marks');
 
-  guy.addEventListener('dragstart', function(e) {
+  guy.on('dragstart', function(e) {
+    e = e.originalEvent;
     dragX = e.x - e.target.x,
     dragY = e.y - e.target.y;
   });
 
   window.addEventListener('drop', function(e) {
-    document.body.appendChild(guy);
-    guy.className = '';
-    guy.style.position = 'absolute';
-    guy.style.left = (e.x - dragX) + 'px';
-    guy.style.top = (e.y - dragY) + 'px';
+    marks.append(guy);
+    guy.removeClass('dragme');
+    guy.css({
+      'position': 'absolute',
+      'left': (e.x - dragX) + 'px',
+      'top': (e.y - dragY) + 'px',
+    });
 
     $.post('marks/', {
-      'x': parseFloat(guy.style.left),
-      'y': parseFloat(guy.style.top),
+      'x': parseFloat(guy.css('left')),
+      'y': parseFloat(guy.css('top')),
     });
   });
 
