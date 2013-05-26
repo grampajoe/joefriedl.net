67248b3ccb89390f74e3b07d779b9753052b6e85
Joe Friedl <E001868@dealertrack.com>
2013-05-25 20:17:19 -0400

    Make dragging work better

--- joefriedl/static/mark.js
+++ joefriedl/static/mark.js
@@ -1,15 +1,26 @@
 function mark() {
   var dragX, dragY,
+      dragee,
       guy = $('#avatar'),
       marks = $('#marks');
 
   guy.on('dragstart', function(e) {
+    dragging = true;
     e = e.originalEvent;
     dragX = e.x - e.target.x,
     dragY = e.y - e.target.y;
   });
 
+  guy.on('dragend', function(e) {
+    dragging = false;
+  });
+
   window.addEventListener('drop', function(e) {
+    // Stop if something else is being dragged
+    if (!dragging) {
+      return;
+    }
+
     marks.append(guy);
     guy.removeClass('dragme');
     guy.css({
